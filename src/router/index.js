8224fdf13138e39b63436336657320f57c155753
noobakong <2492161235@qq.com>
2018-07-27 22:00:02 +0800

    project init

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/pages/home/Home.vue'
+import List from '@/pages/list/List.vue'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/List',
+      name: 'List',
+      component: List
+    }
+  ]
+})
