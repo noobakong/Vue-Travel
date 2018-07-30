df64ef3e2f8b6d93c4cb914508400471f3f84044
noobakong <2492161235@qq.com>
2018-07-30 22:08:04 +0800

    city header

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl';
+  .header
+    position relative
+    overflow hidden
+    height $headerHeight
+    line-height $headerHeight
+    text-align center
+    color #fff
+    background-color $bgcolor
+    font-size .32rem
+    .header-back
+      position absolute
+      top 0
+      left .1rem
+      text-align center
+      font-size .4rem
+      color #fff
+</style>
